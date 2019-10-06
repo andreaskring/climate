47c9d01455a71ac4bbe34f9ae6225221722302e1
Andreas Kring <andreas.kring@gmail.com>
2019-10-06 11:43:04 +0200

    Added simulator; moved Docker things to docker folder

---
+++ utils/simulator.py
@@ -0,0 +1,21 @@
+import random
+import requests
+import time
+
+
+PUSHGW_URL = 'http://localhost:9091/metrics/job/climate/instance/simulator'
+
+
+random.seed()
+
+while True:
+    temp = 20 + 3*random.random()
+    rh = 40 + 3*random.random()
+
+    r_temp = requests.post(PUSHGW_URL, 'temp %s\n' % temp)
+    r_rh = requests.post(PUSHGW_URL, 'rh %s\n' % rh)
+    print('HTTP status (temp = {}): {}'.format(temp, r_temp.status_code))
+    print('HTTP status (rh = {})  : {}'.format(rh, r_rh.status_code))
+    print(35*'-')
+
+    time.sleep(1)
